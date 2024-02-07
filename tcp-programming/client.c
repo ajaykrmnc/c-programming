28ca9bcb88965a82472af2ac4d1e1a7744c01809
ajaykrmnc <ajaykg6917@gmail.com>
2024-02-07 19:38:53 +0530

    3rd day

---
+++ tcp-programming/client.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+// Maximum number of data that can trasfer
+#define MESSAGE_LENGTH 1024
+#define PORT 8888
+
+// Creating Macro for the socketaddr as SA
+#define SA struct sockaddr
+
+//Global Data
+int socket_file_descriptor, connection;
+
+struct sockaddr_in serveraddress, client;
+char message[MESSAGE_LENGTH];
+
+int main(){
+    // Socket Creation
+    socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_file_descriptor == -1){
+        printf("Creating of Socket failed");
+    }
+    // Erase the memory 
+    bzero(&serveraddress, sizeof(serveraddress));
+
+    // Setting the Server Properties
+    serveraddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Setting the port number
+    serveraddress.sin_port = htons(PORT);
+
+    // Protocol family 
+    serveraddress.sin_family = AF_INET;
+
+    connection = connect(socket_file_descriptor, (SA *)&serveraddress, sizeof(serveraddress));
+
+    if(connection == -1){
+        printf("Connection with the server failed\n");
+        exit(1);
+    }
+
+    // Interacting with the server
+    while(1){
+        bzero(message, sizeof(message));
+        printf("Enter the message you want to send to the server: ");
+        scanf("%[^\n]%*c", message);
+        if((strncmp(message, "end", 3)) == 0){
+            write(socket_file_descriptor, message, sizeof(message));
+            printf("Client Exit.\n");
+        }
+        // Sending the data to the server by storing the number
+
+        ssize_t bytes = write(socket_file_descriptor, message, sizeof(message));
+
+        // IF the number of bytes is >= 0 then data is sent
+
+        if(bytes >= 0){
+            printf("Data sned to the server successfully. !\n");
+        }
+        bzero(message, sizeof(message));
+        // Reading the response from the server.
+        read(socket_file_descriptor, message, sizeof(message));
+        printf("Data received from server: %s\n", message);
+    }
+    // Closing the Socket Connection
+    close(socket_file_descriptor);
+    return 0;
+}
